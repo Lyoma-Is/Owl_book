e2e2b996e7da80fff2e47149523249fa4c06f272
lyoma-isp <lyomaikt@gmail.com>
2025-02-01 23:52:55 +0300

    t10

--- js/taskOutput.js
+++ js/taskOutput.js
@@ -1,45 +1,50 @@
-// Подключаем файл JSON
 
-const taskTen = './src/oge_inf/taskTen.json';
+const taskTen = '/src/oge_inf/taskTen.json'; // Проверьте путь!
 
-// Используем fetch для получения содержимого файла
 fetch(taskTen)
-  .then((response) => response.text())
-  .then((data) => {
-    // Преобразуем строку JSON в объект
-    const taskTenData = JSON.parse(data);
-
-    // Выводим данные в HTML
-    let taskTenOutput = '';
-    for (const item of taskTenData) {
-      switch(item.typeTask){
-        case 1: 
-        taskTenOutput += `   
-        <tr>
-          <td style="vertical-align: top;">
-            <div class="number-block">${item.taskNumber}</div>   
-          </td>   
-
-          <td style="vertical-align: top; padding-left: 20px;">
-            <p class="p-num">№ ${item.taskCounter}</p>
-            <p> ${item.task}</p>
-            <hr class="hr-pd_20">
-            <details>
-                <summary class="p-num">Решение</summary>
-                <hr class="hr-pd_10">
-                ${item.taskAnswer}
-            </details>  
-            <hr class="hr-pd_40">
-          </td> 
-        </tr>  
-      `;
-      break;
-
-      }
-      
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`Ошибка: ${response.status} (${response.statusText})`);
     }
-    document.querySelector('.task-numbers__block').innerHTML = taskTenOutput;
+    return response.text();
   })
-  //.catch((error) => console.error('Ошибка:', error));
-
+  .then((data) => {
+    try {
+      const taskTenData = JSON.parse(data);
+      let taskTenOutput = '';
+      for (const item of taskTenData) {
+        switch (item.typeTask) {
+          case 1:
+            taskTenOutput += `   
+              <tr>
+                <td style="vertical-align: top;">
+                  <div class="number-block">${item.taskNumber}</div>   
+                </td>   
+                <td style="vertical-align: top; padding-left: 20px;">
+                  <p class="p-num">№ ${item.taskCounter}</p>
+                  <p>${item.task}</p>
+                  <hr class="hr-pd_20">
+                  <details>
+                    <summary class="p-num">Решение</summary>
+                    <hr class="hr-pd_10">
+                    ${item.taskAnswer}
+                  </details>  
+                  <hr class="hr-pd_40">
+                </td> 
+              </tr>  
+            `;
+            break;
+          // Добавьте другие case, если есть другие типы задач
+        }
+      }
+      // Вставьте сгенерированный HTML в нужный элемент
+      document.querySelector('.task-numbers__block').innerHTML = taskTenOutput;
+    } catch (e) {
+      console.error('Ошибка при парсинге JSON:', e);
+    }
+  })
+  .catch((error) => {
+    console.error('Ошибка при загрузке файла:', error);
+  });
   //Переведите число <b>1011101</b> из двоичной системы счисления в десятичную систему счисления. В ответе запишите полученное число.
+
