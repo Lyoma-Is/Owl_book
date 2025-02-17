7267684cf653ec463e59675fde21bec0a755fb4b
lyoma-isp <lyomaikt@gmail.com>
2025-02-17 21:29:57 +0300

    sort

--- js/taskOutput.js
+++ js/taskOutput.js
@@ -1,6 +1,6 @@
 
-//const taskTen = '/src/oge_inf/taskTen.json'; // Проверьте путь!
-const taskTen = 'https://raw.githubusercontent.com/Lyoma-Is/Owl_book/refs/heads/main/src/oge_inf/taskTen.json'; // Проверьте путь!
+const taskTen = '/src/oge_inf/taskTen.json'; // Проверьте путь!
+//const taskTen = 'https://raw.githubusercontent.com/Lyoma-Is/Owl_book/refs/heads/main/src/oge_inf/taskTen.json'; // Проверьте путь!
 
 fetch(taskTen)
   .then((response) => {
@@ -13,9 +13,12 @@ fetch(taskTen)
   .then((data) => {
     try {
       const taskTenData = JSON.parse(data);
-      let taskTenOutput = '';
-
-      const taskNumber = taskTenData[0].taskNumber;
+      
+       let dt = taskTenData.length
+       console.log(dt-1)
+       
+      //let taskTenOutput = '';
+      //const taskNumber = taskTenData[0].taskNumber;
     
       document.getElementById("taskType").addEventListener("change", filterTasks);
       document.getElementById("taskSort").addEventListener("change", filterTasks);
@@ -45,15 +48,16 @@ fetch(taskTen)
             filteredTasks = taskTenData.filter(task => task.typeTask === selectedType);
         }
         displayTasks(filteredTasks); 
-        console.log(selectedType, sortedTasks)
+        
      }
 
       function displayTasks(filteredTasks) {
         const output = document.querySelector('.task-numbers__block');
         let taskTenOutput = '';
-
+        
         filteredTasks.forEach( item => {
                 switch (item.typeTask) {
+                  
                   case 1:
                       taskTenOutput +=`
                         <p class="p-num">№ ${item.taskCounter}</p>
@@ -88,14 +92,52 @@ fetch(taskTen)
                           <hr class="hr-pd_20">
                     `
                     break;
+                  case 3:
+                      let summaAns = 0;
+                      const num1 = parseInt(item.taskAnswer[0], 2);  // Двоичное число
+                      const num2 = parseInt(item.taskAnswer[1], 8);  // Восьмеричное число
+                      const num3 = parseInt(item.taskAnswer[2], 16); // Шестнадцатеричное число
+
+                      const operator1 = item.taskAnswer[3]; // Первый оператор
+                      const operator2 = item.taskAnswer[4]; // Второй оператор
+
+                      summaAns = num1 + (operator1 === "+" ? num2 : -num2) + (operator2 === "+" ? num3 : -num3);
+                      taskTenOutput +=`
+                        <p class="p-num">№ ${item.taskCounter}</p>
+                          <p>${item.task}</p>
+                          <hr class="hr-pd_10">
+                          <span class="span-centr">${item.taskAnswer[0]}<sub>2</sub> ${item.taskAnswer[3]} ${item.taskAnswer[1]}<sub>8</sub> ${item.taskAnswer[4]} ${item.taskAnswer[2]}<sub>16</sub></span>
+                          <hr class="hr-pd_10">
+                          <p>${item.task2}</p>
+                          <hr class="hr-pd_20">
+                          <details>
+                            <summary class="p-num">Решение</summary>
+                            <hr class="hr-pd_10">
+                            ${item.taskAnswer[0]}<sub>2</sub> = ${parseInt(item.taskAnswer[0], 2)}<sub>10</sub>
+                            <hr class="hr-pd_10">
+                            ${item.taskAnswer[1]}<sub>8</sub> = ${parseInt(item.taskAnswer[1], 8)}<sub>10</sub>
+                            <hr class="hr-pd_10">
+                            ${item.taskAnswer[2]}<sub>16</sub> = ${parseInt(item.taskAnswer[2], 16)}<sub>10</sub>
+                            <hr class="hr-pd_20">
+                            ${num1} ${operator1} ${num2} ${operator2} ${num3} = ${summaAns}
+                            <hr class="hr-pd_20">
+                            Ответ: ${summaAns}
+                          </details>  
+                          <hr class="hr-pd_20">
+                          <hr class="hr-between">
+                          <hr class="hr-pd_20">
+                    `
+                    break;
                   }
+                  //console.log(item.taskCounter)
               }); 
-              output.innerHTML = taskTenOutput;   
+              output.innerHTML = taskTenOutput; 
+              
+                     
       }  
        
     } catch (e) { console.error('Ошибка при парсинге JSON:', e); }
     filterTasks()
-    
   });
   
 
